0889e8ad123173c8c6298f450633209382bbb5b9
NuclearNova <vioeugen@yahoo.com>
2020-04-06 02:39:03 +0300

    Adding application logic

---
+++ ApplicationLogic/DataModel/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLogic.DataModel
+{
+    public class Account
+    {
+        public Guid AccountID { get; set; }
+        public string IBAN { get; set; }
+        public string Balance { get; set; }
+        public ICollection<Card> Cards { get; set; }
+        public ICollection<Transaction> Transactions { get; set; }
+    }
+}
---
+++ ApplicationLogic/DataModel/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLogic.DataModel
+{
+    public class Client
+    {
+        public Guid ClientID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string CNP { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string District { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public ICollection<Account> Accounts { get; set; }
+
+
+    }
+}
