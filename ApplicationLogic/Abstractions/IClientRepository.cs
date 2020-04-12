daea32d35a5bff2d137168ba5ef1512070b6d6f5
serbanelyan <serbanelyan@yahoo.com>
2020-04-13 00:26:24 +0300

    Added Transaction View

---
+++ ApplicationLogic/Abstractions/IClientRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLogic.Abstractions
+{
+    public class IClientRepository
+    {
+    }
+}
---
+++ ApplicationLogic/DataModel/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLogic.DataModel
+{
+    public class Card
+    {
+        public enum CardType
+        {
+            MasterCard,
+            VISA
+        }
+        public Guid CardID { get; set; }
+        public string OwnerName { get; set; }
+        public string SerialNumber { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string CVV { get; set; }
+        public CardType Type { get; set; }
+        
+    }
+}
