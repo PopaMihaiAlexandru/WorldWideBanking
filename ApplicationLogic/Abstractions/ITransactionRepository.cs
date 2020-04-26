051a940016377f569e7c5fb377b7b9d8d03e0f0c
serbanelyan <serbanelyan@yahoo.com>
2020-04-26 21:03:56 +0300

    Added Transaction Service

---
+++ ApplicationLogic/Abstractions/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationLogic.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLogic.Abstractions
+{
+    public interface ITransactionRepository : IRepository<Transaction>
+    {
+        Client GetTransactionByUserId(Guid userId);
+    }
+}
---
+++ DataAccess/Repositories/TransactionRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationLogic.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+
+namespace DataAccess.Repositories
+{
+    public class TransactionRepository : ITransactionRepository 
+    {
+        //public TransactionRepository(BankDbContext dbContext) : base(dbContext)
+        //{
+
+        //}
+        //    public Transaction GetTransactionByUserId(Guid userId)
+        //    {
+        //        var transaction = dbContext.Transactions
+        //                     .Where(t => t.TransactionID == userId)
+        //                     .SingleOrDefault();
+        //        return transaction;
+        //    }
+    }
+}
