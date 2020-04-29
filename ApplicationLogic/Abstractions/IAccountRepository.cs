709de36acc019a6a52c5db7c98827870f22fc831
im-perry <perisoara.mihai@gmail.com>
2020-04-29 22:27:59 +0300

    Client and Account controllers

---
+++ ApplicationLogic/Abstractions/IAccountRepository.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationLogic.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationLogic.Abstractions
+{
+    public interface IAccountRepository : IRepository<Account>
+    {
+        Account GetAccountByUserId(Guid userId);
+    }
+}
