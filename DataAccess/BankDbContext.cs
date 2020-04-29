a2c622bb067c9350744094082b75a1b40b294fbe
im-perry <61919070+im-perry@users.noreply.github.com>
2020-04-29 20:24:45 +0300

    Add files via upload

---
+++ DataAccess/BankDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationLogic.DataModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    public class BankDbContext : DbContext
+    {
+        public BankDbContext(DbContextOptions<BankDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}
+
