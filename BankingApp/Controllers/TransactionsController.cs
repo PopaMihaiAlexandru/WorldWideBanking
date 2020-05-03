f9738abed0666376fccb502bd52f87fc59f66aab
serbanelyan <serbanelyan@yahoo.com>
2020-05-04 00:22:26 +0300

    Added transaction methods

--- BankingApp/Controllers/TransactionsController.cs
+++ BankingApp/Controllers/TransactionsController.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using ApplicationLogic.DataModel;
 using DataAccess;
+using ApplicationLogic.DataModel;
 
 namespace BankingApp.Controllers
 {
--- DataAccess/Repositories/TransactionRepository.cs
+++ DataAccess/Repositories/TransactionRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ApplicationLogic.Abstractions;
+using ApplicationLogic.DataModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,42 @@ namespace DataAccess.Repositories
 {
     public class TransactionRepository : ITransactionRepository 
     {
-        //public TransactionRepository(BankDbContext dbContext) : base(dbContext)
-        //{
+        public TransactionRepository(BankDbContext dbContext) : base(dbContext)
+        {
 
-        //}
-        //    public Transaction GetTransactionByUserId(Guid userId)
-        //    {
-        //        var transaction = dbContext.Transactions
-        //                     .Where(t => t.TransactionID == userId)
-        //                     .SingleOrDefault();
-        //        return transaction;
-        //    }
+        }
+
+        public ApplicationLogic.DataModel.Transaction Add(ApplicationLogic.DataModel.Transaction itemToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(ApplicationLogic.DataModel.Transaction itemToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ApplicationLogic.DataModel.Transaction> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Transactions.Transaction GetTransactionByUserId(Guid userId)
+            {
+                var transaction = dbContext.Transactions
+                             .Where(t => t.TransactionID == userId)
+                             .SingleOrDefault();
+                return transaction;
+            }
+
+        public ApplicationLogic.DataModel.Transaction Update(ApplicationLogic.DataModel.Transaction itemToUpdate)
+        {
+            throw new NotImplementedException();
+        }
+
+        Client ITransactionRepository.GetTransactionByUserId(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
